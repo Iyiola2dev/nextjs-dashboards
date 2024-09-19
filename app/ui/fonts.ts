687740ab4e08ff4d6c71210afdecd28cab45efcf
Lexicon <iyiolad21@gmail.com>
2024-09-19 13:31:57 +0100

    Refactor nav-links component and add fonts module

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana,Bellefair } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const manrope = Bellefair({
+    subsets: ["latin"],
+    weight: "400"
+});
+export const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
